b9774498781ea40ca676cef7030f82a8cc1dcb73
Sendzeagahara <padyshka0@gmail.com>
2024-07-18 04:07:23 +0300

    Screen caption added

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import pygame
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption('Space Ship')
+running = True
 
-while True:
+while running:
     #event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
