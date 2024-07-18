dca4c04f344826e28a3e9d4ce93b6a635d7fbe02
Sendzeagahara <padyshka0@gmail.com>
2024-07-18 03:59:45 +0300

    Basic setup

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('hello world')
+import pygame
+
+#general setup
+pygame.init()
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+while True:
+    #event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    #update screen
+    pygame.display.update()
+
+pygame.quit()
